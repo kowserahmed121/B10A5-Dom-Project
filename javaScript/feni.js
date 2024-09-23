273ff54194825151a97457bd6f47fa0a6ae21c7f
kowserahmed121 <kowser.dev@gmail.com>
2024-09-24 00:53:28 +0600

    add function for sum total donation ammount for fani

--- javaScript/feni.js
+++ javaScript/feni.js
@@ -1,3 +1,19 @@
-document.getElementById("feni-btn").addEventListener("click", function(){
-    
-})
+document.getElementById("feni-btn").addEventListener("click", function () {
+  const inputValue = getInputValueById("feni-input-flied");
+  const textMoneyValue = getTextValueById("feni-total-amount");
+  const totalMoneyValue = getTextValueById("total-amount");
+
+  if (inputValue > 0 && !isNaN(inputValue)) {
+    if (inputValue > totalMoneyValue) {
+      alert("you need money");
+      return;
+    }
+    const newbalance = totalMoneyValue - inputValue;
+    document.getElementById("total-amount").innerText = newbalance;
+
+    const feniBalance = textMoneyValue + inputValue;
+    document.getElementById("feni-total-amount").innerText = feniBalance;
+  } else {
+    alert("not ok");
+  }
+});
--- javaScript/function.js
+++ javaScript/function.js
@@ -3,3 +3,9 @@ function getInputValueById(id){
     const inputFloatValue = parseFloat(inputValue);
     return inputFloatValue;
 }
+
+function getTextValueById(id){
+    const textValue = document.getElementById(id).innerText;
+    const textFloatValue = parseFloat(textValue);
+    return textFloatValue;
+}
